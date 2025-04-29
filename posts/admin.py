0f3fecad541007d7b298297164ee01d8ac1a1df5
Aleksey <arekabu@yandex.com>
2025-04-29 14:29:37 +0300

    Molule_26_Final_Task

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from .models import Author, Post, Category, Comment
 
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'rating']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type', 'author', 'rating', 'get_categories', 'date']
+    list_filter = ('rating', 'date', 'author', 'type', 'category__name')
+    search_fields = ('text', 'title')
+
+    def get_categories(self, post):
+        return ", ".join([category.name for category in post.category.all()])
+
+    get_categories.short_description = 'Categories'
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_filter = ('subscribers',)
+    search_fields = ('name',)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post__title', 'user', 'text', 'rating', 'date']
+    list_filter = ('rating', 'date', 'user')
+    search_fields = ('text',)
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
